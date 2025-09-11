d886ee263a76c14d82c944c05ade12b35ffff74e
apoorvagadkari <58619549+apoorvagadkari@users.noreply.github.com>
2025-09-10 23:41:02 -0400

    Fix CORS for Github pages

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ app.use(cors({
   origin: process.env.NODE_ENV === 'production' 
     ? [
 	'https://apoorvagadkari.github.io',
+	'https://todo-app1-backend.onrender.com',
 	'http://localhost:3000'
 	] 
     : ['http://localhost:3000'],
